cc1f825d7baacccc6ee90ff10cfee45d8351ea20
dreamerjdw <dreamerjdw2013@163.com>
2017-03-28 18:54:34 +0800

    carousel 001

---
+++ carousel/src/bookLogic.js
@@ -0,0 +1,58 @@
+(function() {
+	
+	// --- URL 検索書籍--- //
+	var GET_BOOKLIST = 'src/booklist.json';
+
+	/**
+	 * 書籍ロジック
+	 *
+	 * @class
+	 * @name bookLogic
+	 */
+	var bookLogic = {
+
+		/**
+		 * コントローラ名
+		 *
+		 * @memberOf JDWeb.book.BookController
+		 * @type string
+		 */
+		__name: 'JDWeb.book.BookLogic',
+
+		getBookListData: function() {
+
+			var df = this.deferred();
+
+			$.ajax({
+				url: GET_BOOKLIST,
+				datatype: 'json',
+				success: function(data) {
+					df.resolve(data);
+				},
+				error: function() {
+					alert('書籍データの読み込みに失敗しました。');
+				}
+			});
+			
+			return df.promise();
+		},
+
+		makeCarouselElement: function($root, data) {
+			var bookList = data;
+			for(var i=0, len=bookList.length; i<len; i++) {
+				var _$item =$('<div class="item"></div>');
+				_$item.css({
+					'background': 'url('+ bookList[i].cover_page_url + ')',
+					'background-size': '100%',
+					'float': 'left'
+				});
+
+				$root.append(_$item);
+			}
+		}
+	};
+
+	// グローバルに公開する
+	h5.core.expose(bookLogic);
+
+}) ();
